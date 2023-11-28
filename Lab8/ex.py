2709fda6abd55e81cebb5d28145ab773a6b2d6b9
Iulia <83005332+iuliamoisa@users.noreply.github.com>
2023-11-28 11:23:19 +0200

    ex1

--- Lab8/ex.py
+++ Lab8/ex.py
@@ -5,29 +5,27 @@ import arviz as az
 import matplotlib.pyplot as plt
 from multiprocessing import freeze_support
 def main():
-    # Încărcăm datele
+    # incarcare date
     data = pd.read_csv('Lab8/Prices.csv')
 
-    # Definim variabilele
+    # def variabile
     price = data['Price']
     speed = data['Speed']
     hard_drive = np.log(data['HardDrive'])
 
     with pm.Model() as model:
-        # Definim distribuțiile a priori
+        # distributii a priori
         alpha = pm.Normal('alpha', mu=0, sigma=10)
         beta1 = pm.Normal('beta1', mu=0, sigma=10)
         beta2 = pm.Normal('beta2', mu=0, sigma=10)
         sigma = pm.HalfNormal('sigma', sigma=1)
 
-        # Definim media
+        # media
         mu = alpha + beta1*speed + beta2*hard_drive
 
-        # Definim distribuția a posteriori
+        # distributia a posteriori
         y = pm.Normal('y', mu=mu, sigma=sigma, observed=price)
-
-
-        idata = pm.sample(2000, tune=2000, return_inferencedata=True) # sample-ul, adica distributia a posteriori 
+        idata = pm.sample(2000, tune=2000, return_inferencedata=True)  
 
     az.plot_trace(idata, var_names=['alpha', 'beta1', 'beta2', 'sigma'])
 
